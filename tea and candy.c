0c2ba420a13c7f46cbf01f37233937fa01ea0453
prithisri12 <prithisri35@gmail.com>
2026-02-11 15:14:57 +0530

    Add files via upload

---
+++ tea and candy.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+int tea;
+int candy;
+printf("enter the amount of tea and candy \n");
+scanf("%d%d",&tea,&candy);
+if(tea<=5||candy<=5)
+{
+printf("0");
+}
+else if(tea>=2*candy||candy>=2*tea)
+{
+   printf("2|");
+   }
+   else
+   {
+   printf("1");
+   }
+   return 0;
+   }
